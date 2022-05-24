695aa5ffd8bac705bdf3760469fe5f7408f38ac4
pgm-bramcriel <bramcrie@student.arteveldehs.be>
2022-05-24 17:14:04 +0200

    added music player

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Cd from '../components/cd/Cd';
 import BaseLayout from '../layouts/BaseLayout';
+import dynamic from 'next/dynamic'
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 const Home = () => {
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [trackDuration, setTrackDuration] = useState(0);
+
+  const handleProgress = (progress) => {
+    if (progress.playedSeconds === trackDuration && tracks.length - 1 > trackIndex) {
+      setTrackIndex(trackIndex + 1);
+    }
+  }
+
+  const handleDuration = (duration) => {
+    setTrackDuration(duration);
+  }
+
+  const tracks = [
+    'https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3',
+    'https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3'
+  ]
+
   return (
     <BaseLayout>
-      home
       <Cd />
+      <ReactPlayer
+        playing={true}
+        onProgress={handleProgress}
+        onDuration={handleDuration}
+        controls
+        url={tracks[trackIndex]} />
     </BaseLayout>
   )
 }
