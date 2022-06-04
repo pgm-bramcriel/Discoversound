a435a6af91da372e4131684f720cce76e059f195
pgm-bramcriel <bramcrie@student.arteveldehs.be>
2022-06-05 01:20:06 +0200

    added file upload to firebase storage

--- pages/create.jsx
+++ pages/create.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import BaseTitle from '../components/baseTitle/BaseTitle'
 import MainButton from '../components/mainButton/MainButton'
 import BaseLayout from '../layouts/BaseLayout'
 import Image from 'next/image';
 import { CreateContainer, CreateContent } from './styles/create'
 import { useAuth } from '../context/AuthContext';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../config/firebase';
 
 const Create = () => {
   const image = require("../public/images/undraw_Compose_music_re_wpiw.svg");
   const {user} = useAuth();
+  const [file, setFile] = useState('');
+  const [progress, setProgress] = useState(0);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!file) return;
+    const storageRef = ref(storage, `/songs/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', (snapshot) => {
+      const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+      setProgress(progress);
+    }, (err) => console.log(err),
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref)
+      .then(url => console.log(url))
+    }
+    )
+  }
+
+  const handleFile = (e) => {
+    e.preventDefault();
+    setFile(e.target.files[0]);
+  }
   return (
     <>
       {user &&
@@ -16,7 +42,7 @@ const Create = () => {
           <CreateContainer>
             <BaseTitle title={'sound'} subtitle={'create'}/>
             <CreateContent>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <label>Song name</label>
                 <input
                   type='text'
@@ -24,6 +50,7 @@ const Create = () => {
                 />
                 <label>Song file</label>
                 <input
+                  onChange={handleFile}
                   type='file'
                   placeholder='examplefile.mp3'
                 />
@@ -33,6 +60,7 @@ const Create = () => {
                   placeholder='examplefile.mp3'
                 />
                 <MainButton>Upload!</MainButton>
+                <span>{progress} %</span>
               </form>
               <section>
                 <Image
