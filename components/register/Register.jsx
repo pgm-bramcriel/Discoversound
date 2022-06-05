76d5cc61458578870f52473933c004d1cc588bb8
pgm-bramcriel <bramcrie@student.arteveldehs.be>
2022-06-05 14:28:04 +0200

    added account on register

--- components/register/Register.jsx
+++ components/register/Register.jsx
@@ -1,6 +1,7 @@
 import { updateProfile } from 'firebase/auth';
+import { addDoc, collection } from 'firebase/firestore';
 import React, {useState} from 'react'
-import { auth } from '../../config/firebase';
+import { auth, db } from '../../config/firebase';
 import { useAuth } from '../../context/AuthContext';
 import MainButton from '../mainButton/MainButton'
 import { RegisterStyled } from './style'
@@ -9,7 +10,10 @@ const Register = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [artistName, setArtistName] = useState();
   const {register, user} = useAuth();
+  const artistsDocRef = collection(db, 'artists');
+
 
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -18,6 +22,10 @@ const Register = () => {
       updateProfile(auth.currentUser, {
         displayName: username
       });
+      addDoc(artistsDocRef, {
+        artistName: artistName,
+        userId: auth.currentUser.uid
+      })
     } catch (error) {
       console.log(error);
     }
@@ -30,6 +38,7 @@ const Register = () => {
         <form onSubmit={handleRegister}>
           <div>
             <input onChange={(e) => setUsername(e.target.value)} type='text' placeholder='Username'></input>
+            <input onChange={(e) => setArtistName(e.target.value)} type='text' placeholder='Artist Name'></input>
             <input onChange={(e) => setEmail(e.target.value)} type='email' placeholder='Email'></input>
             <input onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Password'></input>
           </div>
